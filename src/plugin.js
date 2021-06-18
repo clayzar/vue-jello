db5466c2124055338b98f150a505a8ce29668c5c
Clayton Salazar <clayton@thunder.digital>
2021-06-18 12:32:19 -0500

    added option to apply global $models attribute

--- src/plugin.js
+++ src/plugin.js
@@ -2,7 +2,13 @@ import Collection from './components/Collection'
 
 export default {
 	install(Vue, options = {}) {
-		const name = options.name || 'jello-collection'
-		Vue.component(name, Collection)
+		console.log('options:', options);
+
+		const componentName = options.componentName || 'jello-collection'
+		Vue.component(componentName, Collection)
+
+		if(options.models) {
+			Vue.prototype.$models = options.models
+		}
 	}
 }
